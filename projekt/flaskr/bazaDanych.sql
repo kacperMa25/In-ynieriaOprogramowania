30f39825049dafa858d85cf67a3c8bda78eeca2e
kacper <mk55552@zut.edu.pl>
2025-05-19 12:20:39 +0200

    Podpięcie bazy danych

---
+++ projekt/flaskr/bazaDanych.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS employees;
+
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE employees (
+    employeeCode INTEGER PRIMARY KEY AUTOINCREMENT,
+    lastName TEXT NOT NULL,
+    firstName TEXT NOT NULL,
+    login TEXT NOT NULL,
+    reportsTo INTEGER,
+    jobTitle TEXT NOT NULL,
+    FOREIGN KEY (reportsTo) REFERENCES employees (employeeCode)
+);
+
+CREATE TABLE products (
+    productCode INTEGER PRIMARY KEY AUTOINCREMENT,
+    productName TEXT NOT NULL,
+    productLine TEXT NOT NULL,
+    productScale TEXT NOT NULL,
+    productVendor TEXT NOT NULL,
+    productDescription TEXT NOT NULL,
+    quantityInStock INTEGER NOT NULL,
+    buyPrice DECIMAL(10, 2) NOT NULL,
+    MSRP DECIMAL(10, 2) NOT NULL
+);
+
+INSERT INTO
+    employees (lastName, firstName, login, jobTitle)
+VALUES
+    ("admin", "admin", "admin", "admin");
