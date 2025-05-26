9dc0ff385a57bdbd7e03b51281bad48416d07ca9
kacper <mk55552@zut.edu.pl>
2025-05-26 10:52:46 +0200

    venv

--- projekt/flaskr/dashboard.py
+++ projekt/flaskr/dashboard.py
@@ -3,14 +3,14 @@ from flask.templating import render_template
 
 bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 
-@bp.route("/usersPane")
-def usersPane():
+@bp.route("/users")
+def users():
     return render_template("dashboard/users.html")
 
-@bp.route("/productsPane")
-def productsPane():
+@bp.route("/products")
+def products():
     return render_template("dashboard/products.html")
 
 @bp.route("/index")
 def index():
-    return render_template("")
+    return render_template("dashboard/index.html")
