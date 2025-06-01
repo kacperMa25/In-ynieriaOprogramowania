fc5c70771c4c1035d8d66f0c361b11d96dfade22
kacper <mk55552@zut.edu.pl>
2025-06-01 21:26:58 +0200

    Pare dodatkowych pól w dodawaniu przedmiotu

--- projekt/flaskr/bazaDanych.sql
+++ projekt/flaskr/bazaDanych.sql
@@ -1,22 +1,25 @@
 DROP TABLE IF EXISTS employees;
-
 DROP TABLE IF EXISTS products;
 
 CREATE TABLE employees (
     employeeCode INTEGER PRIMARY KEY AUTOINCREMENT,
-    lastName TEXT NOT NULL,
-    firstName TEXT NOT NULL,
-    login TEXT NOT NULL,
-    password TEXT NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    firstName VARCHAR(50) NOT NULL,
+    login VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     reportsTo INTEGER,
-    jobTitle TEXT,
+    jobTitle VARCHAR(50),
     FOREIGN KEY (reportsTo) REFERENCES employees (employeeCode)
 );
 
 CREATE TABLE products (
-    productCode TEXT PRIMARY KEY,
+    productCode VARCHAR(20) PRIMARY KEY,
     productName TEXT NOT NULL,
     productDescription TEXT NOT NULL,
+    category VARCHAR(30),
+    unit VARCHAR(20),
+    location VARCHAR(50),
     quantityInStock INTEGER NOT NULL,
+    minimalQuantity INTEGER,
     price DECIMAL(10, 2) NOT NULL
 );
--- projekt/flaskr/dashboard.py
+++ projekt/flaskr/dashboard.py
@@ -3,13 +3,13 @@ from flaskr.db import get_db
 from flask import (
     Blueprint,
     flash,
-    g,
     redirect,
     render_template,
     request,
-    session,
     url_for,
 )
+
+from werkzeug.security import generate_password_hash
 
 bp = Blueprint("dashboard", __name__, url_prefix="/")
 
@@ -19,7 +19,7 @@ bp = Blueprint("dashboard", __name__, url_prefix="/")
 def index():
     db = get_db()
     wDetails = db.execute(
-        "SELECT COUNT(productCode) as howMany, SUM(quantityInStock * price) as howMuch FROM products"
+        "SELECT COUNT(productCode) as howMany, SUM(quantityInStock * price) as howMuch, SUM(quantityInStock < minimalQuantity) as lowStock FROM products"
     ).fetchone()
     return render_template("dashboard/index.html", wDetails=wDetails)
 
@@ -34,9 +34,40 @@ def users():
     return render_template("dashboard/users.html", wDetails=wDetails)
 
 
-@bp.route("/users/add")
+@bp.route("/users/add", methods=("GET", "POST"))
 @loginRequired
 def add_user():
+    if request.method == "POST":
+        db = get_db()
+        firstName = request.form["firstName"]
+        lastName = request.form["lastName"]
+        login = request.form["login"]
+        password = request.form["password"]
+        error = None
+
+        if not firstName or firstName.strip() == "":
+            error = "Imie jest wymagane"
+        elif not lastName or lastName.strip() == "":
+            error = "Nazwisko jest wymagane"
+        elif not login or login.strip() == "":
+            error = "Login jest wymagany"
+        elif not password or password.strip() == "":
+            error = "Hasło jest wymagane"
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO employees (firstName, lastName, login, password) VALUES (?, ?, ?, ?)",
+                    (firstName, lastName, login, generate_password_hash(password)),
+                )
+                db.commit()
+
+            except db.IntegrityError as e:
+                error = f"{e}"
+            else:
+                return redirect(url_for("index"))
+
+        flash(error)
     return render_template("dashboard/add-user.html")
 
 
@@ -45,7 +76,7 @@ def add_user():
 def products():
     db = get_db()
     wDetails = db.execute(
-        "SELECT SUM(quantityInStock) as howMany, COUNT(DISTINCT productCode) as howManyD FROM products"
+        "SELECT SUM(quantityInStock) as howMany, COUNT(productCode) as howManyD FROM products"
     ).fetchone()
     return render_template("dashboard/products.html", wDetails=wDetails)
 
@@ -59,6 +90,10 @@ def add_item():
         itemName = request.form["itemName"]
         itemCode = request.form["sku"]
         itemDesc = request.form["description"]
+        itemCategory = request.form.get("category", "")
+        itemUnit = request.form.get("unit", "")
+        itemLocation = request.form["location"]
+        itemMinQuant = request.form["minStock"]
         itemQuantity = request.form["quantity"]
         itemPrice = request.form["price"]
 
@@ -68,15 +103,24 @@ def add_item():
             error = "Kod wymagany"
         elif not itemDesc or itemDesc.strip() == "":
             error = "Opis wymagany"
-        elif not itemQuantity or itemQuantity.strip() == "":
+        elif not itemQuantity:
             error = "Ilość wymagana"
-        elif not itemPrice or itemPrice.strip() == "":
+        elif not itemPrice:
             error = "Cena wymagana"
+        elif not itemCategory:
+            error = "Kategoria wymagana"
+        elif not itemUnit:
+            error = "Jednostka wymagana"
+        elif not itemLocation:
+            error = "Lokalizacja wymagana"
+        elif not itemMinQuant:
+            error = "Minimala ilość wymagana"
         else:
             try:
                 quantity = int(itemQuantity)
                 price = float(itemPrice)
-                if quantity <= 0:
+                minStock = int(itemMinQuant)
+                if quantity or minStock <= 0:
                     error = "Ilość musi być większa niż 0"
                 elif price <= 0:
                     error = "Cena musi być większa niż 0"
@@ -85,8 +129,18 @@ def add_item():
 
             try:
                 db.execute(
-                    "INSERT INTO products (productCode, productName, productDescription, quantityInStock, price) VALUES (?, ?, ?, ?, ?)",
-                    (itemCode, itemName, itemDesc, itemQuantity, itemPrice),
+                    "INSERT INTO products (productCode, productName, productDescription, category, unit, location, quantityInStock, minimalQuantity, price) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    (
+                        itemCode,
+                        itemName,
+                        itemDesc,
+                        itemCategory,
+                        itemUnit,
+                        itemLocation,
+                        itemQuantity,
+                        itemMinQuant,
+                        itemPrice,
+                    ),
                 )
                 db.commit()
             except db.IntegrityError as e:
@@ -97,3 +151,9 @@ def add_item():
         flash(error)
 
     return render_template("dashboard/add-item.html")
+
+
+@bp.route("/products/edit", methods=("GET", "POST"))
+@loginRequired
+def editItem():
+    return render_template("dashboard/edit-item.html")
